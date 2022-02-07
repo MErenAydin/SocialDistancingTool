e13549ac3127fb1a31ab372afdfe125cad89816d
Muhammet Eren Aydın <merenep12@gmail.com>
2022-02-07 14:52:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,28 @@ import imutils
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
    
-cap = cv2.VideoCapture("test_assets/test.mp4")
+cap = cv2.VideoCapture("test_assets/test2.mp4")
    
 while cap.isOpened():
     # Reading the video stream
     ret, image = cap.read()
     if ret:
 
+        # resizing for better performance but it reduces frame rate
         #image = imutils.resize(image, width=min(400, image.shape[1]))
+        # Detecting on grayscale image is faster.
         gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
    
-        # Detecting all the regions 
-        # in the Image that has a 
-        # pedestrians inside it
-        (regions, _) = hog.detectMultiScale(gray,
-                                            winStride=(4, 4),
-                                            padding=(0, 0),
-                                            scale=1.05)
+        # Detecting all the regions in the Image that has a human inside it
+        (regions, _) = hog.detectMultiScale(gray, winStride=(4, 4), padding=(4, 4), scale=1.05)
    
-        # Drawing the regions in the 
-        # Image
+        # Drawing the regions in the coloured image
         for (x, y, w, h) in regions:
-            cv2.rectangle(image, (x, y),
-                          (x + w, y + h), 
-                          (0, 0, 255), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 1)
    
         # Showing the output Image
-        cv2.imshow("Image", image)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.imshow("Output", image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
